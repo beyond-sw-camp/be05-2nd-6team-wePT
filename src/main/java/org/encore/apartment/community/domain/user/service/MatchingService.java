3573f0617e5df5e4a2ef90e40e2f5c8982e6a75f
mhshin <tlsalsgud2@naver.com>
2024-03-20 17:57:17 +0900

    chore: temporal commit

--- src/main/java/org/encore/apartment/community/domain/user/service/MatchingService.java
+++ src/main/java/org/encore/apartment/community/domain/user/service/MatchingService.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.encore.apartment.community.domain.user.data.dto.MatchingDto;
+import org.encore.apartment.community.domain.user.data.dto.RequestMatchingDto;
 
 import io.swagger.v3.oas.models.responses.ApiResponse;
 
 public interface MatchingService {
 
-	public void insertMatchingInfo(MatchingDto params);
+	public void insertMatchingInfo(RequestMatchingDto params);
 
 	public Optional<ApiResponse> findApartmentInfo(Long id);
 
