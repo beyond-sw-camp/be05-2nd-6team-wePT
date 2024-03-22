ed8eb927101312131c6ee7251f89bc564e1ff9bd
mhshin <tlsalsgud2@naver.com>
2024-03-22 18:40:58 +0900

    wrap up commit

--- src/main/java/org/encore/apartment/community/domain/matching/data/dto/RequestMatchingDto.java
+++ src/main/java/org/encore/apartment/community/domain/matching/data/dto/RequestMatchingDto.java
@@ -1,41 +1,29 @@
 package org.encore.apartment.community.domain.matching.data.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 import org.encore.apartment.community.domain.matching.data.entity.Matching;
 
-import lombok.Getter;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 @Data
 public class RequestMatchingDto {
 
+	@NotBlank
+	private Long matchingMatchingCategoryId;
 
+	@NotBlank
+	private String matchingOwnerId;
 
-    @NotBlank
-    private Integer matching_matchingCategory_id;
+	@NotBlank
+	private Integer matchingHeadCountLimit;
 
-    @NotBlank
-    private String matchingOwnerId;
+	public static Matching toEntity(RequestMatchingDto dto) {
+		return Matching.builder()
+			.matchingMatchingId(dto.matching_matchingCategory_id)
+			.matchingOwnerId(dto.matchingOwnerId)
+			.matchingHeadCountLimit(dto.matchingHeadCountLimit)
+			.build();
 
-    @NotBlank
-    private Integer matchingHeadCountLimit;
-
-
-
-
-
-
-
-
-    public static Matching toEntity(RequestMatchingDto dto) {
-        return Matching.builder()
-                .matchingMatchingId(dto.matching_matchingCategory_id)
-                .matchingOwnerId(dto.matchingOwnerId)
-                .matchingHeadCountLimit(dto.matchingHeadCountLimit)
-                .build();
-
-    }
-
-
+	}
 
 }
--- src/main/java/org/encore/apartment/community/domain/matching/service/MatchingServiceImpl.java
+++ src/main/java/org/encore/apartment/community/domain/matching/service/MatchingServiceImpl.java
@@ -3,10 +3,10 @@ package org.encore.apartment.community.domain.matching.service;
 import java.util.List;
 import java.util.Optional;
 
-
 import org.encore.apartment.community.domain.matching.data.dto.RequestMatchingDto;
 import org.encore.apartment.community.domain.matching.data.dto.ResponseMatchingDto;
 import org.encore.apartment.community.domain.matching.data.dto.UpdateMatchingDto;
+import org.encore.apartment.community.domain.matching.data.entity.Matching;
 import org.encore.apartment.community.domain.matching.data.repository.MatchingRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,23 +20,40 @@ public class MatchingServiceImpl implements MatchingService {
 
 	private final MatchingRepository matchingRepository;
 
+	public MatchingServiceImpl(MatchingRepository matchingRepository) {
+		this.matchingRepository = matchingRepository;
+	}
+
 	@Override
 	public void insertMatchingInfo(RequestMatchingDto params) {
-
+		Matching matching = RequestMatchingDto.toEntity(params);
+		matchingRepository.save(matching);
+		log.info("insertMatchingInfo = {}", matching);
 	}
 
 	@Override
 	public Optional<ResponseMatchingDto> findMatchingInfo(Long id) {
-		return Optional.empty();
+		// Optional<Matching> matching = MatchingRepository.findById(id);
+		// log.info("findMatchingInfo = {}", matching);
+		//
+		// return matching.map(ResponseMatchingDto::new);
+		return null;
 	}
 
 	@Override
 	public List<ResponseMatchingDto> findMatchingInfoList() {
+		// List<Matching> matchingList = MatchingRepository.findAll();
+		// log.info("findApartmentInfoList = {}", matchingList);
+		//
+		// return aptList.stream().map(ResponseApartmentDto::new).collect(Collectors.toList());
 		return null;
 	}
 
 	@Override
 	public void updateMatchingInfoById(Long id, UpdateMatchingDto params) {
+		// Matching matching = MatchingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 매칭 정보가 없습니다."));
+		// matching.update(matching.getMatchingMatchingCategoryId(), matching.getApartmentAddress(), apartment.getApartmentTotalHousehold());
+		// log.info("updateApartmentInfo = {}", params);
 
 	}
 
