3d601aa635d638f634054f3273e26f6cb54a76b2
mhshin <tlsalsgud2@naver.com>
2024-03-26 15:49:54 +0900

    MatchingStatus insert error has been revised. (insert method worked simular to update method since there was no pk(idx). so I added idx as pk in MatchingStatus)

--- src/main/java/org/encore/apartment/community/domain/matchingStatus/data/entity/MatchingStatus.java
+++ src/main/java/org/encore/apartment/community/domain/matchingStatus/data/entity/MatchingStatus.java
@@ -1,31 +1,40 @@
 package org.encore.apartment.community.domain.matchingStatus.data.entity;
 
-
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MatchingStatus {
 
-    @Id
-    @Column(name = "matching_status_matching_id")
-    private Long matchingStatusMatchingId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "matchingStatus_idx")
+	private Long matchingStatusIdx;
 
-    @Column(name = "matching_status_follower_id")
-    private String matchingStatusFollowerId;
+	@Column(name = "matchingStatus_matching_id")
+	private Long matchingStatusMatchingId;
 
-    @Builder
-    public MatchingStatus(Long matchingStatusMatchingId, String matchingStatusFollowerId) {
-        this.matchingStatusMatchingId = matchingStatusMatchingId;
-        this.matchingStatusFollowerId = matchingStatusFollowerId;
-    }
+	@Column(name = "matchingStatus_follower_id")
+	private String matchingStatusFollowerId;
 
-    public void update(String matchingStatusFollowerId) {
+	@Builder
+	public MatchingStatus(Long matchingStatusMatchingId, String matchingStatusFollowerId) {
+		this.matchingStatusMatchingId = matchingStatusMatchingId;
+		this.matchingStatusFollowerId = matchingStatusFollowerId;
+	}
 
-        this.matchingStatusFollowerId = matchingStatusFollowerId;
-    }
+	public void update(String matchingStatusFollowerId) {
+
+		this.matchingStatusFollowerId = matchingStatusFollowerId;
+	}
 }
 
