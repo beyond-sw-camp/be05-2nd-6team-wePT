6df68f5c3d83387e295f43738bd67018af292888
Su Hwan CHOI <hap_hazard@naver.com>
2024-03-27 04:51:31 +0900

    feat: refresh 토큰을 활용한 로그인 기능 구현

---
+++ src/main/java/org/encore/apartment/community/domain/auth/repository/UserRefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package org.encore.apartment.community.domain.auth.repository;
+
+import java.util.Optional;
+
+import org.encore.apartment.community.domain.auth.dto.entity.UserRefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+	Optional<UserRefreshToken> findByUserIdxAndReissueCountLessThan(Long userIdx, long reissueCount);
+}
